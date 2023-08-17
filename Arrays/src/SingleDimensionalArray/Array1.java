58fbb735921d25f60503cf15950a9a5b50041ca3
PeddolluDeva <peddolludeva803@gmail.com>
2023-08-17 19:05:05 +0530

    DynamicallyArray

---
+++ Arrays/src/SingleDimensionalArray/Array1.java
@@ -0,0 +1,10 @@
+package SingleDimensionalArray;
+
+public class Array1 {
+public static void main(String[] args) {
+	int arr[] = {50,29,38,22,78};
+	for(int  i=0;i<=arr.length;i++) {
+		System.out.print(arr[i]);
+	}
+}
+}
